ab197ba81ff695d37734c6f95e3ef4e866e37d6d
Abraham <abrahamalfonzo11gmail.com>
2024-06-06 20:35:22 -0400

    Funciones de descargar aplicadas(mp3 como mp4), Con un estilo simple(mejorar),

--- index.py
+++ index.py
@@ -2,28 +2,43 @@ from flask import *
 from pytube import YouTube
 from moviepy.editor import *
 import os
+
+#Iniciar Server
 app = Flask(__name__)
 
 @app.route('/download', methods=["GET","POST"])
+
 def home():
    if request.method == "POST":
-        video = request.form['video']
-        yt = YouTube(video)
-        v = yt.streams.get_audio_only().download()
-        audio = AudioFileClip(v)
-        v_mp3 = audio.write_audiofile(audio.filename.replace('mp4','mp3'))
-        ruta_vmp3 = os.path.abspath(v_mp3)
-        return send_file(ruta_vmp3,as_attachment=True)
-        
-    
+         if request.form['audio'] :
+            link_mp3 = request.form['audio']
+            download_mp3 = download_audio(link_mp3)
+            return download_mp3
+         
+         if request.form['video'] :
+            
+            link_mp4 = request.form['video']
+            download_mp4 = download_video(link_mp4)
+            return download_mp4
    return render_template('index.html')
 
-
+# Descargar en formato mp3
+def download_audio(link_audio):
+      yt = YouTube(link_audio) 
+      v = yt.streams.get_audio_only().download()
+      v_mp3 = v.replace('mp4','mp3')
+      v_audio = v_mp3
+      audio = AudioFileClip(v)
+      audio.write_audiofile(audio.filename.replace('mp4','mp3'))
+      os.remove(v)
+      return send_file(v_mp3,as_attachment=True)
    
-    
+#Descargar en formato mp4
+def download_video(link_video): 
+   yt = YouTube(link_video)
+   v = yt.streams.first().download()
+   return send_file(v, as_attachment=True)   
 
- 
-  
 if __name__ == '__main__':
     app.run()
 
