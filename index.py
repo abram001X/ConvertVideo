ac921d556e5bbb667e5dbb81490a45056922e7a9
Abraham <abrahamalfonzo11gmail.com>
2024-08-04 07:23:10 -0400

    variables added

--- index.py
+++ index.py
@@ -2,87 +2,91 @@ from flask import *
 from pytubefix import YouTube,Search
 from pytubefix.cli import on_progress
 from moviepy.editor import *
-#Iniciar Server
-app = Flask(__name__)
+from dotenv import load_dotenv
+def crear_app():
+   #Iniciar Server
+   app = Flask(__name__)
 
-#Ruta de busqueda con info
-@app.route('/apjsdabram/<url>')
-def  search_url(url):
-   array = url.split(",")
-   yt = download_audio_video(array[0],f'https://www.youtube.com/watch?v={array[1]}')
-   return send_file(yt, as_attachment=True)
+   #Ruta de busqueda con info
+   @app.route('/apjsdabram/<url>')
+   def  search_url(url):
+      array = url.split(",")
+      yt = download_audio_video(array[0],f'https://www.youtube.com/watch?v={array[1]}')
+      return send_file(yt, as_attachment=True)
 
 
-#Ruta de busqueda
-@app.route('/search', methods=['GET','POST'])
-def search():
-   if request.method == 'POST':
-      res = request.form['search']
-      res = res.replace(" ","")
-      if res == "":
-         return render_template("search.html")
-      result = Search(res)
-      print("erorr", result)
-      obj = result.results
-      ob = []  
-      if len(obj) > 5:
-         for i in range(0,5):
-            ob.append(obj[i])
-      else :
-         for i in range(0,len(obj)):
-            ob.append(obj[i])
-      return render_template('search.html', dato = ob, cadena = res)
-   
-   return render_template('search.html')
-
-#Ruta principal
-@app .route('/', methods=['GET','POST'])
-def home():
-   if request.method == "POST":
-      if request.form['link']: 
-         link = request.form['link']
-         caracters = link.find("https://www.youtube.com")
-         if caracters == 0 :
-            date = convert_url(link)
-            return render_template('download.html', dato = date)
+   #Ruta de busqueda
+   @app.route('/search', methods=['GET','POST'])
+   def search():
+      if request.method == 'POST':
+         res = request.form['search']
+         res = res.replace(" ","")
+         if res == "":
+            return render_template("search.html")
+         result = Search(res)
+         print("erorr", result)
+         obj = result.results
+         ob = []  
+         if len(obj) > 5:
+            for i in range(0,5):
+               ob.append(obj[i])
          else :
-            return render_template("download.html", regex = "Escribe un link válido")  
-   return render_template('download.html')
+            for i in range(0,len(obj)):
+               ob.append(obj[i])
+         return render_template('search.html', dato = ob, cadena = res)
+      
+      return render_template('search.html')
 
-@app .errorhandler(404)
-def page_error(e):
-   return render_template('page_error.html')
+   #Ruta principal
+   @app .route('/', methods=['GET','POST'])
+   def home():
+      if request.method == "POST":
+         if request.form['link']: 
+            link = request.form['link']
+            caracters = link.find("https://www.youtube.com")
+            if caracters == 0 :
+               date = convert_url(link)
+               return render_template('download.html', dato = date)
+            else :
+               return render_template("download.html", regex = "Escribe un link válido")  
+      return render_template('download.html')
 
-#Convert Url
-def convert_url(link):
-   yt = YouTube(link)
-   date = {
-         'title' : yt.title,  
-         'url' : yt.thumbnail_url,
-         'views': yt.views,
-         'autor': yt.author,
-         'link' : link,
-         'duration' : str(round(yt.length/60,2)).replace('.',':'),
-         'autor': yt.author,
-         'fecha' : str(yt.publish_date).replace('00:00:00',''),
-         'video_id' : yt.video_id
-         }
-   return date
+   @app .errorhandler(404)
+   def page_error(e):
+      return render_template('page_error.html')
 
-#Descargar audio_video usando YouTube()
-def download_audio_video(format,url):
-   yt = YouTube(url, on_progress_callback = on_progress)
-   if format == 'mp4':
-      v = yt.streams.get_highest_resolution().download()
-      return v
-   if format == 'mp3':
-      v = yt.streams.get_audio_only().download(mp3=True)
-      return v
+   #Convert Url
+   def convert_url(link):
+      yt = YouTube(link)
+      date = {
+            'title' : yt.title,  
+            'url' : yt.thumbnail_url,
+            'views': yt.views,
+            'autor': yt.author,
+            'link' : link,
+            'duration' : str(round(yt.length/60,2)).replace('.',':'),
+            'autor': yt.author,
+            'fecha' : str(yt.publish_date).replace('00:00:00',''),
+            'video_id' : yt.video_id
+            }
+      return date
 
+   #Descargar audio_video usando YouTube()
+   def download_audio_video(format,url):
+      yt = YouTube(url, on_progress_callback = on_progress)
+      if format == 'mp4':
+         v = yt.streams.get_highest_resolution().download()
+         return v
+      if format == 'mp3':
+         v = yt.streams.get_audio_only().download(mp3=True)
+         return v
+      
+   return app
 
-#Descargar audio_video usando Search
-#def download_mp4_mp3(format,url):
-   
+   #Descargar audio_video usando Search
+   #def download_mp4_mp3(format,url):
+
 if __name__ == '__main__':
-   app.run(port=5000)
+   app = crear_app()
+   app.run()
    
