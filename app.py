f07ddca9b4711f27559150dbf6681fff6045747e
Abraham <abrahamalfonzo11gmail.com>
2024-08-04 13:41:26 -0400

    fixed requirements

--- app.py
+++ app.py
@@ -3,22 +3,21 @@ from pytubefix import YouTube,Search
 from pytubefix.cli import on_progress
 from moviepy.editor import *
 from dotenv import load_dotenv
-from index import app
-def crear_app():
+
    #Iniciar Server
-   app = Flask(__name__)
+app = Flask(__name__)
 
    #Ruta de busqueda con info
-   @app.route('/apjsdabram/<url>')
-   def  search_url(url):
+@app.route('/apjsdabram/<url>')
+def  search_url(url):
       array = url.split(",")
       yt = download_audio_video(array[0],f'https://www.youtube.com/watch?v={array[1]}')
       return send_file(yt, as_attachment=True)
 
 
    #Ruta de busqueda
-   @app.route('/search', methods=['GET','POST'])
-   def search():
+@app.route('/search', methods=['GET','POST'])
+def search():
       if request.method == 'POST':
          res = request.form['search']
          res = res.replace(" ","")
@@ -39,8 +38,8 @@ def crear_app():
       return render_template('search.html')
 
    #Ruta principal
-   @app .route('/', methods=['GET','POST'])
-   def home():
+@app .route('/', methods=['GET','POST'])
+def home():
       if request.method == "POST":
          if request.form['link']: 
             link = request.form['link']
@@ -52,12 +51,12 @@ def crear_app():
                return render_template("download.html", regex = "Escribe un link válido")  
       return render_template('download.html')
 
-   @app .errorhandler(404)
-   def page_error(e):
+@app .errorhandler(404)
+def page_error(e):
       return render_template('page_error.html')
 
    #Convert Url
-   def convert_url(link):
+def convert_url(link):
       yt = YouTube(link)
       date = {
             'title' : yt.title,  
@@ -73,18 +72,16 @@ def crear_app():
       return date
 
    #Descargar audio_video usando YouTube()
-   def download_audio_video(format,url):
+def download_audio_video(format,url):
       yt = YouTube(url, on_progress_callback = on_progress)
+      print('objeto',yt)
       if format == 'mp4':
          v = yt.streams.get_highest_resolution().download()
          return v
       if format == 'mp3':
          v = yt.streams.get_audio_only().download(mp3=True)
          return v
-      
-   return app
 
 if __name__ == '__main__':
-   app = crear_app()
    app.run()
    
